2403803ee6b570e694b7111e3d3b68d325b1b79c
Nicolaia2d <nikri3209@oslomet.no>
2025-03-23 13:27:54 +0100

    endert h1d

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const themeToggle = document.getElementById('themeToggle');
+    themeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
+});
